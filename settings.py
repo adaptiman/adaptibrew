2d235eb1db455bd92e6b5454b76914df1c7006af
David Sweeney <sweeney@tamu.edu>
2016-11-20 22:27:47 -0600

    created some test files

---
+++ settings.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#this file is used to store common variables for the adaptibrew configuration
+port = "/dev/ttyAMA0"
+rimsaddress = 1
+switchaddress = 2
+baudrate = 19200
+MA0 = "55" #master start byte
+MA1 = "AA" #master1 byte
+MAE = "77" #master end byte
---
+++ str116.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import serial
+import time
+
+port = "/dev/ttyAMA0"
+usart = serial.Serial (port,9600)
+bytestring = "55AA0817020008012A77"
+message_bytes = bytestring.decode("hex")
+usart.write(message_bytes)
+time.sleep(5)
+message_bytes = "55AA0817020008002977".decode("hex")
+usart.write(message_bytes)
