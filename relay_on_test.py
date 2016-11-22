cb02951a076fa40534a9aacadcff3d1359aca816
David Sweeney <sweeney@tamu.edu>
2016-11-22 16:28:31 -0600

    added zfill to cs routine

--- relay_on_test.py
+++ relay_on_test.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
 import str116
 
-str116.relay_on(5)
+# 0 is off, 1 is on
+str116.relay_toggle(0, 0)
--- str116.py
+++ str116.py
@@ -5,20 +5,28 @@ def _write_message(data):
     usart = serial.Serial (settings.port,settings.baudrate)
     message_bytes = data.decode("hex")
     usart.write(message_bytes)
+
 def _get_checksum(data):
     checksum = sum(bytearray.fromhex(data))
     checksumstripped = hex(checksum).replace('0x', '')
-    return checksumstripped
+    return checksumstripped.zfill(2)
 
-def relay_on(relaynumber):
+def relay_toggle(relaynumber, onoff):
+    # if onoff == 'on':
+    #     onoff = '01'
+    #     else
+    #     onoff = '00'
+    #     pass
     #command to turn on relay is 0x08 0x17
     #format is
     #MA0, MA1, 0x08, 0x17, CN, start number output (relaynumber), \
-    #number of outputs (usually 0x01), 0/1, CS, MAE
+    #number of outputs (usually 0x01), 01, CS, MAE
     #need to do a checksum on
     #0x08, 0x17, CN, relaynumber, 0x01, 0x01
     relaynumberhex = hex(relaynumber).replace('0x', '').zfill(2)
-    str_to_checksum = '0817' + settings.CN + str(relaynumberhex) + '0101'
+    str_to_checksum = '0817' + settings.CN + str(relaynumberhex) \
+        + '01' + str(onoff).zfill(2)
     CS = _get_checksum(str_to_checksum)
-    bytestring = settings.MA0 + settings.MA1 + str_to_checksum + str(CS.zfill(2)) + settings.MAE
-    _write_message(bytestring)
+    bytestring = settings.MA0 + settings.MA1 + str_to_checksum + str(CS) + settings.MAE
+    print(bytestring)
+#    _write_message(bytestring)
