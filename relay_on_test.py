a50a9efdf38e7652654f8373cc6cf4ff0e91c690
David Sweeney <sweeney@tamu.edu>
2016-11-22 16:42:51 -0600

    cleaned up the relay_toggle function to take both the relay number and off/on (0/1)

--- relay_on_test.py
+++ relay_on_test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import str116
 
-# 0 is off, 1 is on
+# first variable is the relay number in decimal (0-15)
+# second variable is off/on (0,1) in decimal
 str116.relay_toggle(0, 0)
--- str116.py
+++ str116.py
@@ -4,7 +4,10 @@ import settings
 def _write_message(data):
     usart = serial.Serial (settings.port,settings.baudrate)
     message_bytes = data.decode("hex")
-    usart.write(message_bytes)
+    try:
+        usart.write(message_bytes)
+    except IOError as e :
+        print("Failed to write to the port. ({})".format(e))
 
 def _get_checksum(data):
     checksum = sum(bytearray.fromhex(data))
@@ -12,22 +15,16 @@ def _get_checksum(data):
     return checksumstripped.zfill(2)
 
 def relay_toggle(relaynumber, onoff):
-    # if onoff == 'on':
-    #     onoff = '01'
-    #     else
-    #     onoff = '00'
-    #     pass
-    #  a change
     #command to turn on relay is 0x08 0x17
     #format is
     #MA0, MA1, 0x08, 0x17, CN, start number output (relaynumber), \
-    #number of outputs (usually 0x01), 01, CS, MAE
-    #need to do a checksum on
-    #0x08, 0x17, CN, relaynumber, 0x01, 0x01
-    relaynumbezrhex = hex(relaynumber).replace('0x', '').zfill(2)
+    #number of outputs (usually 0x01), 00/01 (off/on), CS (calculated), MAE
+    #need to do a checksum on 0x08, 0x17, CN, relaynumber, 0x01, 0x01
+    relaynumberhex = hex(relaynumber).replace('0x', '').zfill(2)
     str_to_checksum = '0817' + settings.CN + str(relaynumberhex) \
         + '01' + str(onoff).zfill(2)
     CS = _get_checksum(str_to_checksum)
-    bytestring = settings.MA0 + settings.MA1 + str_to_checksum + str(CS) + settings.MAE
+    bytestring = settings.MA0 + settings.MA1 + str_to_checksum \
+        + str(CS) + settings.MAE
     print(bytestring)
-#    _write_message(bytestring)
+    #_write_message(bytestring)
