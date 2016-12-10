b838e0593704bc4db3fd7aae917b7aa7079779b2
David Sweeney <sweeney@tamu.edu>
2016-12-10 15:59:38 -0600

    adds a procedures file

--- str116.py
+++ str116.py
@@ -68,16 +68,17 @@ def get_relay_status():
     print('relay status bytestring: ' + bytestring)
     relaystatus = _write_message_with_response(bytestring)[6:-4]
     print('relay status: ' + relaystatus)
+
+
     totalrelays = len(relaystatus)
-    print('total relays: ' + str(totalrelays/2))
-    print(relaystatus[0:2])
     n = 0
     while(n < totalrelays):
-        if relaystatus[n:2] is '00':
-            print('relay ' + relaystatus[n:2] + ': off')
+        if str(relaystatus[n:n+2]) == '00':
+            print('relay ' + str(n/2).zfill(2) + ': off')
         else:
-            print('relay ' + relaystatus[n:2] + ': on')
-        n += 1
+            print('relay ' + str(n/2).zfill(2) + ': on')
+        n += 2
+
 
 
 def set_baudrate():
