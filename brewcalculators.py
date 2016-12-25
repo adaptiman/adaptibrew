2e7d1e0611d5e1fb3328227045fd9294946e92c1
David Sweeney <sweeney@tamu.edu>
2016-12-25 13:15:06 -0600

    Added a strike temp calculator.

--- brewcalculators.py
+++ brewcalculators.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
 
-def calc_strike_temp():
-    
+def calc_strike_temp(WaterVolInQuarts, GrainMassInPounds, GrainTemp, MashTemp):
+    WaterToGrainRatio = WaterVolInQuarts / GrainMassInPounds
+    StrikeWaterTemp = ((0.2 / WaterToGrainRatio) * (MashTemp - GrainTemp)) + MashTemp
+    return round(StrikeWaterTemp,1)
