5c917b1d5c98d8db97ebfb0d28b8558d1c46d0a3
Luke Sweeney <luke@thesweeneys.org>
2017-03-29 23:05:23 -0500

    adds some comments in the setting.py file

---
+++ set_relay.py
@@ -0,0 +1,5 @@
+import settings
+import str116
+import sys
+
+str116.set_relay(int(sys.argv[1]), int(sys.argv[2]))
--- settings.py
+++ settings.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 #this file is used to store common variables for the adaptibrew configuration
 port = "/dev/ttyAMA0"
+# this is the address of the PID, which is controlled by the minimalmodbus lib
+# than the relays
 rimsaddressint = 1
 switchaddressint = 2
 baudrate = 19200
@@ -9,6 +11,7 @@ MA0 = '55' #master start byte
 MA1 = 'AA' #master1 byte
 MAE = '77' #master end byte
 CN  = '02'  #hex byte for controller number
+# these are relay numbers on the STR116 board and a custom to each installation
 spargeToMashRelay = 0
 spargeRelay = 1
 rimsToMashRelay = 2
