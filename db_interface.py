7ce29802e4f64f8b42f434a0c768d5888e260e92
Luke Sweeney <luke@thesweeneys.org>
2017-06-07 12:20:32 -0500

    removes comments in db_interface and adds sleep (a good one this time)

--- db_interface.py
+++ db_interface.py
@@ -13,9 +13,6 @@ if not Request.table_exists():
     db.create_tables([Request])
 
 while True:
-    exchange.write_latest_data()   # Get data from hardware and write it to db
-    exchange.check_for_requests()  # Check for new requests
-
-    # Checking for new requests takes about 0.003 seconds; not an issue.
-    # Writing the data takes about 2-3 seconds, since gathering the data from the hardware
-    # (i.e. minimalmodbus) takes a long time
+    exchange.write_latest_data()
+    exchange.check_for_requests()
+    time.sleep(0.5)
